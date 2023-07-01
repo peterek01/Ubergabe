3400a080d7424afd8f461163d68705b0b8ecc65a
peterek01 <135986425+peterek01@users.noreply.github.com>
2023-07-01 22:30:17 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const date = document.querySelector("h1 span");
 const now = new Date();
-date.textContent =
-	now.getDate() + "." + now.getMonth() + "." + now.getFullYear();
+const day = now.getDate() < 10? '0' + now.getDate() : now.getDate();
+const month = now.getMonth() < 10 ? '0' + now.getMonth() : now.getMonth;
+const year = now.getFullYear();
+
+date.textContent = `${day}.${month}.${year}`;
